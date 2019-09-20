e17e01f91c5d68083d138afea907797fe1d94cc0
Enoferge <jade.escapist@yandex.ru>
2019-09-20 21:40:49 +0300

    lessons 1-4

---
+++ src/components/Foooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h3><div color="#3AC1EF">This is my footer element</div></h3>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo () {
+    return (
+        <div>
+            <h1>Lana</h1>
+            <p>I have a physical and mental disorders.</p>
+            <ol>
+                <li>Japan</li>
+                <li>USA</li>
+                <li>New Zealand</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo
