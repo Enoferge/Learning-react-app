4329759b1d970ff5147be207a9b6bc973f6c4ce8
Enoferge <jade.escapist@yandex.ru>
2019-09-20 19:27:27 +0300

    Updates

---
+++ precache-manifest.6ad508687024c6cf93f81dd91aa20c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709b8e6dc638421b7dba99b6ffa3355e",
+    "url": "/Learning-react-app/index.html"
+  },
+  {
+    "revision": "69080f273c51d2899166",
+    "url": "/Learning-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "36212995a8b017648ed4",
+    "url": "/Learning-react-app/static/js/2.cd930777.chunk.js"
+  },
+  {
+    "revision": "69080f273c51d2899166",
+    "url": "/Learning-react-app/static/js/main.473ce961.chunk.js"
+  },
+  {
+    "revision": "19e1023ffefa75c143c2",
+    "url": "/Learning-react-app/static/js/runtime-main.722cbcab.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Learning-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
